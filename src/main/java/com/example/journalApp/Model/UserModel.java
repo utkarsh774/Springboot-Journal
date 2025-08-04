7cbb773dfad194f7c392fddc3c0a93e054d78f60
Utkarsh Singh <72231578+utkarsh774@users.noreply.github.com>
2025-08-04 15:36:37 +0530

    Fix circular dependency between security config and JWT filter

--- src/main/java/com/example/journalApp/Model/UserModel.java
+++ src/main/java/com/example/journalApp/Model/UserModel.java
@@ -1,16 +1,29 @@
 package com.example.journalApp.Model;
 
-
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Document(collection = "users")
-public class UserModel {
+public class UserModel implements UserDetails {
 
+    @Id
     private String id;
     private String name;
     private String email;
     private String password;
 
+    public UserModel() {
+    }
+
+    public UserModel(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
     public String getId() {
         return id;
@@ -36,11 +49,42 @@ public class UserModel {
         this.email = email;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
